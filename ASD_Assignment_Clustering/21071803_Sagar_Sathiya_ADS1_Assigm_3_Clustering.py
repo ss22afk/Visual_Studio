4550519e73524c4f45f83b3e61dc9855568a2561
ss22afk <ss22afk@herts.ac.uk>
2023-01-20 13:16:31 +0000

    done

--- ASD_Assignment_Clustering/21071803_Sagar_Sathiya_ADS1_Assigm_3_Clustering.py
+++ ASD_Assignment_Clustering/21071803_Sagar_Sathiya_ADS1_Assigm_3_Clustering.py
@@ -13,7 +13,7 @@ from sklearn import preprocessing
 import seaborn as sns
 
 def exp_growth(t, scale, growth):
-    f = scale * np.exp(growth * (t-1990))
+    f = scale * np.exp(growth * (t-1960))
     return f
 def logistics(t, scale, growth, t0):
     ''' 
@@ -163,7 +163,6 @@ def applyDBSCANalgo(dataframe, min_sample_pre, col_Name_1, col_Name_2):
     plt.xlabel(col_Name_2)
     plt.ylabel(col_Name_1)
     plt.title('Density-based(DBSCAN algorithm) Clustering.')
-    #plt.show()
 
 # https://data.worldbank.org/indicator/EG.ELC.ACCS.ZS
 # Access to electricity (% of population)
@@ -186,6 +185,16 @@ print('Normalized Value: ', norm_access_electricity_arr)
 scaled_access_electricity_df = pd.DataFrame(data=per_population_acc_elect_arr, columns=['Scaled_Access_to_electricity_per_population'])
 print('\n Scaled Data: \n', scaled_access_electricity_df.head(10))
 
+grp_average_by_year = rmvna_access_electricity_df.groupby('Years', as_index=False).mean()
+
+plt.figure()
+plt.plot(grp_average_by_year['Years'], grp_average_by_year['Access to electricity (% of population)'], linestyle='dashed')
+plt.title('Access to elecricity average.')
+plt.xlabel('Years')
+plt.ylabel('Access to electricity (% of population)')
+plt.xticks(grp_average_by_year['Years'], rotation=90)
+plt.show()
+
 # Group a data by year and calculated yearly average of data.
 grp_access_electricity_per_popu = rmvna_access_electricity_df.groupby('Years', as_index=False).mean()
 print('Group data set: \n', grp_access_electricity_per_popu.head(10))
@@ -199,14 +208,13 @@ print('\n Data type of dataFrame: ',rmvna_access_electricity_df.dtypes)
 
 #fit exponential  growth
 popt, covar = opt.curve_fit(exp_growth, rmvna_access_electricity_df['Years'],rmvna_access_electricity_df['Access to electricity (% of population)'])
-print('Fit parameter: ', popt)
+print('Fit parameter: ', popt) #Fit parameter:  [-1.05093743e-20  9.99999993e-01]
 
 # use *popt to pass on the fit parameters
 rmvna_access_electricity_df['pop_exp'] = exp_growth(rmvna_access_electricity_df['Years'], *popt)
 plt.figure()
 plt.plot(rmvna_access_electricity_df['Years'], rmvna_access_electricity_df['Access to electricity (% of population)'], label = 'data')
 plt.plot(rmvna_access_electricity_df['Years'], rmvna_access_electricity_df['pop_exp'], label='fit')
-
 plt.title("First fit")
 plt.xlabel('year')
 plt.ylabel('Access to electricity (% of population)')
@@ -217,7 +225,7 @@ print()
 # Try scaling with the 1950 population and a smaller exponential factor
 # decrease or increase exponential factor until rough agreement is reached
 # growth of 0.02 gives a reasonable start value
-popt = [4e8, 0.01]
+popt = [4e1, 0.05]
 rmvna_access_electricity_df["pop_exp"] = exp_growth(rmvna_access_electricity_df["Years"], *popt)
 plt.figure()
 plt.plot(rmvna_access_electricity_df['Years'], rmvna_access_electricity_df['Access to electricity (% of population)'], label = 'data')
@@ -230,7 +238,7 @@ print()
 
 # fit exponential growth
 popt, covar = opt.curve_fit(exp_growth, rmvna_access_electricity_df["Years"],
-rmvna_access_electricity_df["Access to electricity (% of population)"], p0=[4e8, 0.02])
+rmvna_access_electricity_df["Access to electricity (% of population)"], p0=[4e1, 0.6])
 
 # much better
 print("Fit parameter", popt)
@@ -292,3 +300,4 @@ low, up = err_ranges(2050, logistics, popt, sigma)
 mean = (up+low) / 2.0
 pm = (up-low) / 2.0
 print("2050:", mean, "+/-", pm)
+plt.show()
